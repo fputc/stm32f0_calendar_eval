1d96deb4813694c67580f598642e2a8c7a10959f
Alen Helac <alen.helac@gmail.com>
2015-02-25 20:52:55 +0100

    updated gitignore to ignore build folder

--- main.c
+++ main.c
@@ -1,17 +1,15 @@
 #include "stm32f0xx.h"
 #include "stm32f0xx_gpio.h"
 #include "stm32f0xx_rcc.h"
+#include "stm32f0xx_ah_delay.h"
 #include <stdio.h>
 
 int main(void)
 {
+	AH_DELAY_SysTickInit();
 
     while(1)
     {
+    	AH_DELAY_DelayMs(1000);
     }
-}
-
-signed int fputc(signed int c, FILE *pStream)
-{
-    1 == 1;
 }
