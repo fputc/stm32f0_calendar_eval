614d89490d82c3d2dc044552fdd2045fb241f083
Alen Helac <alen.helac@gmail.com>
2015-02-25 20:44:02 +0100

    init commit

---
+++ main.c
@@ -0,0 +1,17 @@
+#include "stm32f0xx.h"
+#include "stm32f0xx_gpio.h"
+#include "stm32f0xx_rcc.h"
+#include <stdio.h>
+
+int main(void)
+{
+
+    while(1)
+    {
+    }
+}
+
+signed int fputc(signed int c, FILE *pStream)
+{
+    1 == 1;
+}
