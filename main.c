29d599ffc5a454f23393f68f290bb683f9d58078
Alen Helac <alen.helac@gmail.com>
2015-02-25 21:11:30 +0100

    started working on usart

--- main.c
+++ main.c
@@ -1,15 +1,51 @@
 #include "stm32f0xx.h"
 #include "stm32f0xx_gpio.h"
 #include "stm32f0xx_rcc.h"
+#include "stm32f0xx_usart.h"
+
 #include "stm32f0xx_ah_delay.h"
+
 #include <stdio.h>
+
+
+void AH_USART_Init(int baudRate) {
+	USART_InitTypeDef USART_InitStructure;
+	GPIO_InitTypeDef GPIO_InitStructure;
+
+	RCC_AHBPeriphClockCmd(RCC_AHBPeriph_GPIOA, ENABLE);
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_USART1,ENABLE);
+
+	GPIO_PinAFConfig(GPIOA, GPIO_PinSource9, GPIO_AF_1);
+	GPIO_PinAFConfig(GPIOA, GPIO_PinSource10, GPIO_AF_1);
+
+	GPIO_InitStructure.GPIO_Pin =  GPIO_Pin_9 | GPIO_Pin_10;
+	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF;
+	GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
+	GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_UP;
+	GPIO_Init(GPIOA, &GPIO_InitStructure);
+
+	USART_InitStructure.USART_BaudRate = baudRate;
+	USART_InitStructure.USART_WordLength = USART_WordLength_8b;
+	USART_InitStructure.USART_StopBits = USART_StopBits_1;
+	USART_InitStructure.USART_Parity = USART_Parity_No;
+	USART_InitStructure.USART_HardwareFlowControl = USART_HardwareFlowControl_None;
+	USART_InitStructure.USART_Mode = USART_Mode_Rx | USART_Mode_Tx;
+	USART_Init(USART1, &USART_InitStructure);
+
+	USART_Cmd(USART1,ENABLE);
+}
 
 int main(void)
 {
 	AH_DELAY_SysTickInit();
+	AH_USART_Init(9600);
 
     while(1)
     {
-    	AH_DELAY_DelayMs(1000);
+		AH_DELAY_DelayMs(1000);
+		printf("\n\rUSART Printf Example: retarget the C library printf function to the USART\n\r");
+		AH_DELAY_DelayMs(1000);
+		USART_SendData(USART1, 'X');
     }
 }
